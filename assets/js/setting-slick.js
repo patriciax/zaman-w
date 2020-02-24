534a4853fde3d9d2931adb8304484372d22d1cf4
patriciax <yively.pa@gmail.com>
2020-02-24 11:46:50 -0400

    cambios baaner

--- assets/js/setting-slick.js
+++ assets/js/setting-slick.js
@@ -136,3 +136,39 @@ $('.main-iconos__content').slick({
   }
   ]
 });
+
+
+
+$('.main-funciona__content').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  dots:false,
+  arrows: false,
+  responsive: [{
+    breakpoint: 1200,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      infinite: true,
+      dots: true
+    }
+  },
+  {
+    breakpoint: 900,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  },
+  {
+    breakpoint: 600,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+
+      autoplaySpeed: 1000
+    }
+  }
+  ]
+});
