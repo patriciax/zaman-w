8dbf3f7622b8e9e133ea80f325a70fcc534584ee
Gladibeth <gladibethdelosa@gmail.com>
2020-03-12 15:26:14 -0400

    Add view productoras

--- assets/js/setting-slick.js
+++ assets/js/setting-slick.js
@@ -34,6 +34,42 @@ $('.main-clients__content').slick({
 });
 
 
+
+$('.main-clients__content--two').slick({
+  infinite: true,
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  dots: false,
+  arrows: false,
+  responsive: [{
+    breakpoint: 1200,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 3,
+      infinite: true,
+      dots: true
+    }
+  },
+  {
+    breakpoint: 900,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 1
+    }
+  },
+  {
+    breakpoint: 600,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+
+      autoplaySpeed: 1000
+    }
+  }
+  ]
+});
+
+
 $('.main-fotos__content').slick({
   infinite: true,
   slidesToShow: 3,
